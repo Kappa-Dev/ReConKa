25a5558d6b5b61aed1cdab1d18a5b097d30f1d3f
Pierre Boutillier <pierre@hms.harvard.edu>
2017-05-10 14:49:09 -0400

    ReConKa uses Trace.fold_trace_file

--- src/ReConKa.ml
+++ src/ReConKa.ml
@@ -22,52 +22,14 @@ let options = [
 ]
 
 let replay fname =
-  let desc = open_in fname in
-  let lex_buf = Lexing.from_channel desc in
-  let lex_st = Yojson.init_lexer ~fname () in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_lcurl lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let ident = Yojson.Basic.read_ident lex_st lex_buf in
-  let () = assert (ident = "uuid") in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_colon lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let _ = Yojson.Basic.read_string lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_comma lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let ident = Yojson.Basic.read_ident lex_st lex_buf in
-  let () = assert (ident = "env") in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_colon lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let env = Model.of_yojson
-      (Yojson.Basic.read_json lex_st lex_buf) in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_comma lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let ident = Yojson.Basic.read_ident lex_st lex_buf in
-  let () = assert (ident = "trace") in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let () = Yojson.Basic.read_colon lex_st lex_buf in
-  let () = Yojson.Basic.read_space lex_st lex_buf in
-  let rule_names =
-    let size = Model.nb_syntactic_rules env + 1 in
-    Array.init
-      size
-      (Format.asprintf "%a" (Model.print_ast_rule ~env)) in
-  let plot = Output.open_plot
-      ~period:!period ~distances_in_json:(not !raw_distances)
-      rule_names !out_file in
   let progress = Progress_report.create 80 '#' in
   try
-    let final = Yojson.Basic.read_sequence
-        (fun state x y ->
-           let step = Trace.step_of_yojson (Yojson.Basic.read_json x y) in
+    let _,(plot,final) =
+      Trace.fold_trace_file
+        (fun env (plot,state) step ->
            let state',dist = Replay.do_step (Model.signatures env) state step in
            let () = Output.maybe_plot plot state' in
-           let () = match dist with
+           let () = match dist.Replay.unary_distances with
              | None -> ()
              | Some (rule,length) ->
                Output.new_distance
@@ -75,23 +37,27 @@ let replay fname =
                  state'.Replay.time length in
            let () =
              Progress_report.tick
+               ~efficiency:false
                state'.Replay.time 0. state'.Replay.event 0. progress in
-           state')
-        (Replay.init_state ~with_connected_components:true) lex_st lex_buf in
+           plot,state')
+        (fun env ->
+           (let rule_names =
+             let size = Model.nb_syntactic_rules env + 1 in
+             Array.init
+               size
+               (Format.asprintf "%a" (Model.print_ast_rule ~env)) in
+           Output.open_plot
+             ~period:!period ~distances_in_json:(not !raw_distances)
+             rule_names !out_file,
+           Replay.init_state ~with_connected_components:true))
+        fname in
     let () = Progress_report.complete_progress_bar
         final.Replay.time final.Replay.event progress in
-    let () = Yojson.Basic.read_space lex_st lex_buf in
-    let () = try Yojson.Basic.read_object_end lex_buf
-      with Yojson.End_of_object -> () in
-    let () = Yojson.Basic.read_space lex_st lex_buf in
-    let () = close_in desc in
     Output.close_plot plot
   with
   | ExceptionDefn.Internal_Error er
   | ExceptionDefn.Malformed_Decl er ->
     let () = Pp.error Format.pp_print_string er in
-    let () = Output.close_plot plot in
-    let () = close_in desc in
     exit 2
 
 let main () =
